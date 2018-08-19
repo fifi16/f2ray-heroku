9b0b02ddedc9075a7a6e7734c7892dc0f6297d40
fifi16 <31301257+fifi16@users.noreply.github.com>
2018-08-20 07:02:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-cd /v2raybin
-wget -O v2ray.zip http://github.com/fifi16/f2ray-heroku/releases/download/3.11/v2ray-linux-64.zip
-unzip v2ray.zip 
-cd /v2raybin/v2ray-v$VER-linux-64
+cd /f2raybin
+wget -O f2ray.zip http://github.com/fifi16/f2ray-heroku/releases/download/3.11/v2ray-linux-64.zip
+unzip f2ray.zip 
+cd /f2raybin/v2ray-v$VER-linux-64
 chmod +x f2ray
 chmod +x v2ctl
 
