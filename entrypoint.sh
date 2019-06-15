039232e613c38fd5760a21312a93f31acc8814bd
fifi16 <31301257+fifi16@users.noreply.github.com>
2019-06-15 23:41:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@ cd /f2raybin
 wget -O f2ray.zip https://github.com/fifi16/v2ray-core/releases/download/v$VER/f2ray-linux-64.zip
 unzip f2ray.zip 
 cd /f2raybin/v2ray-v$VER-linux-64
-chmod +x f2ray
-chmod +x v2ctl
+chmod +777 f2ray
+chmod +777 v2ctl
 
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
